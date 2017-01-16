5b29fbf78e5df15e1560ed66a9de3a80e2174244
aram ghanipour <aram.ghanipour@hotmail.com>
2017-01-17 00:00:59 +0100

    Edited nav and design

--- BenSeen/ClientApp/app/app.module.ts
+++ BenSeen/ClientApp/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+//import { Ng2PaginationModule } from 'ng2-pagination';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
@@ -20,6 +22,7 @@ import { AuthorComponent } from './components/author/author.component';
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        //Ng2PaginationModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
