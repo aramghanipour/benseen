20d369f52cca2a4a69a105b0852bec3d39830d5f
aram ghanipour <aram.ghanipour@hotmail.com>
2017-01-16 23:18:43 +0100

    Added author component

---
+++ BenSeen/ClientApp/app/components/author/author.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'author',
+    template: require('./author.component.html')
+})
+export class AuthorComponent {
+    public authors: Author[];
+
+    constructor(http: Http) {
+        http.get('http://localhost/bookfinderapi/api/authors/getall').subscribe(result => {
+            this.authors = result.json();
+        });
+    }
+}
+
+interface Author {
+    authorID: number;
+    firstname: string;
+    lastname: string;
+    country:string;
+}
